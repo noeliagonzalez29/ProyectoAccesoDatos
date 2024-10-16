eddb4fc7bcc01e546757385558692a830a4ca4ef
ngonzalezg29 <ngonzalezg31@educarex.es>
2024-10-16 21:31:35 +0200

    menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,52 @@
-public class Main {
-    public static void main(String[] args) {
+import java.nio.file.Files;
+import java.util.Scanner;
 
-        System.out.println("h");
+public class Main {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner entrada= new Scanner(System.in);
+        int opcion;
+        if (Files.exists()) {
+
+            do {
+                System.out.println("-----BIENVENIDO A STARDAM VALLEY------");
+                System.out.println("1. NUEVA PARTIDA");
+                System.out.println("2. CARGAR PARTIDA");
+                opcion = entrada.nextInt();
+                switch (opcion) {
+                    case 1:
+                        nuevaPartida();
+                        break;
+
+                    case 2:
+                        cargarPartida();
+                        break;
+
+                    default:
+                        System.out.println("Opcion no válida");
+                        break;
+
+                }
+
+            } while (opcion != 2);
+        }else {
+            System.out.println("-----BIENVENIDO A STARDAM VALLEY------");
+            System.out.println("1. NUEVA PARTIDA");
+
+            opcion = entrada.nextInt();
+            switch (opcion) {
+                case 1:
+                    nuevaPartida();
+                    break;
+                default:
+                    System.out.println("Opcion no válida");
+                    break;
+
+            }
+
+
     }
 }
